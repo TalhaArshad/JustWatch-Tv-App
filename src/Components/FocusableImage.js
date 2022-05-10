edf54f7d0cda4ec138da8656d01c303d72c17b0d
talharshad <86104183+talharshad@users.noreply.github.com>
2022-05-10 22:34:36 +0500

    image has been made focusable

--- src/Components/FocusableImage.js
+++ src/Components/FocusableImage.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {TouchableOpacity, Text, Image, StyleSheet} from 'react-native';
 
 const FocusableImage = ({imageSrc, title}) => {
+    const [isFocused, setIsFocused] = useState(false)
+    const [shouldDisplayTitle, setShouldDisplayTitle] = useState(false)
   return (
-    <TouchableOpacity style={styles.container}>
-      <Image source={imageSrc} style={styles.image}></Image>
-      <Text style={styles.title}>{title}</Text>
+    <TouchableOpacity 
+    activeOpacity = {1}
+    style={styles.container}
+    onFocus = {()=>{
+        setIsFocused(true)
+    }}
+    onBlur = {()=>{
+        setIsFocused(false)
+    }}
+    onPress = {()=>{
+        setShouldDisplayTitle(true)
+    }}>
+
+      <Image source={imageSrc} style={[styles.image, isFocused && styles.frame]}></Image>
+      <Text style={styles.title}>{shouldDisplayTitle ? title : ''}</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'yellow',
+    width: 400,
+    height: 600,
+    alignSelf: 'center',
+    alignItems: 'center',
+    margin: 60,
+    
+   
+  },
+  frame:{
+    
+    borderColor: 'white'
   },
   image: {
-    width: 350,
+    width: 380,
     height: 500,
+    borderWidth: 10,
+    
   },
   title: {
     fontSize: 30,
-    marginTop: 10,
+    marginTop: 20,
+    color: 'white'
   },
 });
 
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
+import FocusableImage from '../Components/FocusableImage';
 
+const data = [
+  {title: 'Letters to Juliet', image: require('../Assets/Images/pic1.jpeg')},
+  {title: 'The Dictator', image: require('../Assets/Images/pic2.jpeg')},
+];
 const HomeScreen = () => {
-  
+  const renderItem = ({item}) => {
+    return <FocusableImage title={item.title} imageSrc={item.image}/>;
+  };
   return (
-    <View>
-      
+    <View style= {styles.container}>
+      <FlatList 
+        horizontal
+      data={data} renderItem={renderItem}></FlatList>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems:'center',
+        
+    }
+})
 
 export default HomeScreen;
