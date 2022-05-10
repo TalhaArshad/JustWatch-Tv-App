fff879f641895bd2b89e04281a03670a0e9b1ec7
talharshad <86104183+talharshad@users.noreply.github.com>
2022-05-11 01:23:51 +0500

    back button press handler implemented

--- src/Components/FocusableImage.js
+++ src/Components/FocusableImage.js
@@ -1,29 +1,36 @@
-import React,{useState} from 'react';
+import React, {useState, useImperativeHandle} from 'react';
 import {TouchableOpacity, Text, Image, StyleSheet} from 'react-native';
 
-const FocusableImage = ({imageSrc, title}) => {
-    const [isFocused, setIsFocused] = useState(false)
-    const [shouldDisplayTitle, setShouldDisplayTitle] = useState(false)
+const FocusableImage = React.forwardRef((props, ref) => {
+  const {imageSrc, title} = props;
+  const [isFocused, setIsFocused] = useState(false);
+  const [shouldDisplayTitle, setShouldDisplayTitle] = useState(false);
+  useImperativeHandle(ref, () => ({
+    backButtonPressed() {
+      setShouldDisplayTitle(false);
+    },
+  }));
   return (
-    <TouchableOpacity 
-    activeOpacity = {1}
-    style={styles.container}
-    onFocus = {()=>{
-        setIsFocused(true)
-    }}
-    onBlur = {()=>{
-        setIsFocused(false)
-        setShouldDisplayTitle(false)
-    }}
-    onPress = {()=>{
-        setShouldDisplayTitle(true)
-    }}>
-
-      <Image source={imageSrc} style={[styles.image, isFocused && styles.frame]}></Image>
+    <TouchableOpacity
+      activeOpacity={1}
+      style={styles.container}
+      onFocus={() => {
+        setIsFocused(true);
+      }}
+      onBlur={() => {
+        setIsFocused(false);
+        setShouldDisplayTitle(false);
+      }}
+      onPress={() => {
+        setShouldDisplayTitle(true);
+      }}>
+      <Image
+        source={imageSrc}
+        style={[styles.image, isFocused && styles.frame]}></Image>
       <Text style={styles.title}>{shouldDisplayTitle ? title : ''}</Text>
     </TouchableOpacity>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -32,23 +39,19 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
     margin: 60,
-    
-   
   },
-  frame:{
-    
-    borderColor: 'white'
+  frame: {
+    borderColor: 'white',
   },
   image: {
     width: 380,
     height: 500,
     borderWidth: 10,
-    
   },
   title: {
     fontSize: 30,
     marginTop: 20,
-    color: 'white'
+    color: 'white',
   },
 });
 
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  BackHandler,
+  TVEventControl,
+} from 'react-native';
 import FocusableImage from '../Components/FocusableImage';
 
 const data = [
@@ -7,25 +13,44 @@ const data = [
   {title: 'The Dictator', image: require('../Assets/Images/pic2.jpeg')},
 ];
 const HomeScreen = () => {
+  const backHandler = useRef();
+  const backPressRef = useRef();
+  useEffect(() => {
+    TVEventControl.enableTVMenuKey();
+
+    backHandler.current = BackHandler.addEventListener(
+      'hardwareBackPress',
+      function () {
+        backPressRef.current.backButtonPressed();
+      },
+    );
+
+    return () => {
+      backHandler.current.remove();
+    };
+  }, []);
   const renderItem = ({item}) => {
-    return <FocusableImage title={item.title} imageSrc={item.image}/>;
+    return (
+      <FocusableImage
+        ref={backPressRef}
+        title={item.title}
+        imageSrc={item.image}
+      />
+    );
   };
   return (
-    <View style= {styles.container}>
-      <FlatList 
-        horizontal
-      data={data} renderItem={renderItem}></FlatList>
+    <View style={styles.container}>
+      <FlatList horizontal data={data} renderItem={renderItem}></FlatList>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: 'black',
-        alignItems:'center',
-        
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+});
 
 export default HomeScreen;
